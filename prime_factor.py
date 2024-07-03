60bf61372ab4409c01ff1d8a4d0416de85c680be
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-07-04 00:32:36 +0100

    largest prime factor

---
+++ prime_factor.py
@@ -0,0 +1,19 @@
+# helper function
+def is_prime(x):
+    if x <= 1:
+        return False
+    for index in range(2, x):
+        if x%index == 0:
+            return False
+    return True
+
+
+# largest prime factor function
+def largest_prime_factor(number):
+    for index in range(number//2, 2, -1):
+        if number%index == 0:
+            if is_prime(index):
+                print(index)
+                break
+largest_prime_factor(600851475143)
+
