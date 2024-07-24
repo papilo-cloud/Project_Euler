839453ce23a7bdec799347bb1cce006b66d43443
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-07-24 15:37:04 +0100

    lattice path

---
+++ src/lattice_path.py
@@ -0,0 +1,19 @@
+# Factorial number helper function
+def factorial(num):
+    if num <= 1:
+        return 1
+    return num * factorial(num - 1)
+
+
+def lattice_path(k, n):
+    # In a (k x n) grid, the number of lattice path from
+    # coordinates (0,0) to (k,n) is equal to the number
+    # of combinations of (k) objects out of (k+n) options
+    #  n!/(n-k)!(k!)
+    # for our matrix, (n = k = 20)
+    
+    n = k + n
+    routes = factorial(n) // (factorial(k) * factorial(n - k))
+    
+    print(f'routes {routes}')
+lattice_path(20, 20)
